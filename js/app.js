3128b2711c01b5baff402e7a726786397524b667
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2020-09-05 12:50:52 +0100

    updated functionality and media queries

--- js/app.js
+++ js/app.js
@@ -4,31 +4,37 @@ var nextBtn = document.querySelector("#next");
 var screen = document.querySelector("#screen");
 var images = document.querySelectorAll("img");
 var tempPrevQuote = [];
-var tempPrevImage = [];
 var prevQuote = [];
-var prevImage = [];
 var prev;
-var previous;
-var length;
+var tempPrevImg = [];
+var prevImg = [];
+var PImg;
 
 //*********************NEXT BUTTON
 nextBtn.addEventListener("click", () => {
   // console.log(quoteContainer.length);
+
   quoteContainer.forEach((item) => {
     item.classList.remove("show");
   });
+
   images.forEach((item) => {
     item.classList.remove("background");
   });
+
   //Select random Image//
+
   let randomImg = Math.floor(Math.random() * 83 + 1);
+
   let backgroundImg = images[randomImg].classList.add("background");
+  tempPrevImg.push(randomImg);
+
   //Select random Quote//
+
   let random = Math.floor(Math.random() * 263 + 1);
 
   let display = quoteContainer[random].classList.add("show");
   tempPrevQuote.push(random);
-  console.log(tempPrevQuote);
 });
 
 //***********************PREVIOUS BUTTON
@@ -36,25 +42,38 @@ previousBtn.addEventListener("click", () => {
   images.forEach((item) => {
     item.classList.remove("background");
   });
+
   quoteContainer.forEach((item) => {
     item.classList.remove("show");
   });
+
+  prevQuote = tempPrevQuote;
+  prev = prevQuote.pop();
+
   if (prevQuote == undefined) {
     //Select random Quote//
     let random = Math.floor(Math.random() * 263 + 1);
     let display = quoteContainer[random].classList.add("show");
+  } else if (prev == undefined) {
+    let random = Math.floor(Math.random() * 263 + 1);
+    let display = quoteContainer[random].classList.add("show");
   } else {
-    prev = tempPrevQuote;
-    length = prev.length;
-    lengthM = length - 1;
-    console.log(prev);
-    console.log(length--);
-    console.log(lengthM);
-    console.log(prev[lengthM]);
+    quoteContainer[prev].classList.add("show");
   }
+
   //Select random Image//
-  let randomImg = Math.floor(Math.random() * 84);
-  let backgroundImg = images[randomImg].classList.add("background");
+
+  prevImg = tempPrevImg;
+  PImg = prevImg.pop();
+  if (prevImg == undefined) {
+    let randomImg = Math.floor(Math.random() * 84);
+    let backgroundImg = images[randomImg].classList.add("background");
+  } else if (PImg == undefined) {
+    let randomImg = Math.floor(Math.random() * 84);
+    let backgroundImg = images[randomImg].classList.add("background");
+  } else {
+    images[PImg].classList.add("background");
+  }
 });
 
 var date = document.querySelector("#date");
@@ -63,11 +82,15 @@ date.innerHTML = Date();
 
 // NAVBAR
 var main = document.querySelector("#main");
+
 var content = document.querySelector("#main-content");
+
 var navbar = document.querySelector("#toggle-btn");
+
 navbar.addEventListener("click", () => {
   main.classList.toggle("toggle");
 });
+
 content.addEventListener("click", () => {
   main.classList.remove("toggle");
 });
